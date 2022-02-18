8faab4c3d078fadb7b76a9fa2d281d4c1c243ba8
Hazel Bastug <z.hazel.bastug@perkinelmer.com>
2022-02-18 10:56:17 -0500

    added first class

---
+++ src/test/java/pages/sample.java
@@ -0,0 +1,15 @@
+package pages;
+
+/**
+ * Created By Z. Hazel Bastug
+ * Date :2022-02-18
+ * Time :10:55 a.m.
+ * Project Name : tenant-manager
+ */
+
+public class sample {
+
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
